bb383e996b84b3c5a4735016a0308e97a5590deb
mikhailyatsenko <misha.ggg@gmail.com>
2025-02-20 10:23:29 +0100

    add persist, fix some styles

--- src/entities/taskMatrix/ui/quadrant/Quadrant.tsx
+++ src/entities/taskMatrix/ui/quadrant/Quadrant.tsx
@@ -57,7 +57,7 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
   return (
     <div
       ref={setNodeRef}
-      className={`${quadrantColors[quadrantKey]} ${isActive ? '!bg-gray-400' : ''} ${isDimmed ? 'opacity-50' : ''} m-1 ${expandedQuadrant === null ? 'h-40 w-[calc(50%-8px)]' : isExpanded ? 'order-first h-80 w-full !pb-8' : `h-1 w-1`} relative overflow-hidden rounded-md p-6 text-gray-100 transition-all duration-300 ease-in-out sm:h-80 dark:border dark:bg-black ${isAnimateNotExpandedQuadrant ? 'animate-from-hide-to-show' : ''}`}
+      className={`${quadrantColors[quadrantKey]} ${isActive ? '!bg-gray-400' : ''} ${isDimmed ? 'opacity-20' : ''} m-1 ${expandedQuadrant === null ? 'h-40 w-[calc(50%-8px)]' : isExpanded ? 'order-first h-80 w-full !pb-8' : `h-1 w-1`} relative overflow-hidden rounded-md p-6 text-gray-100 transition-all duration-300 ease-in-out sm:h-80 dark:border dark:bg-black ${isAnimateNotExpandedQuadrant ? 'animate-from-hide-to-show' : ''}`}
     >
       <h2 className="absolute top-1 right-2 mb-2 text-[0.5rem] text-gray-600 sm:text-sm dark:text-gray-300">
         {titleMap[quadrantKey]}
@@ -77,7 +77,7 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
             ))}
           </ul>
           <div
-            className={`flex h-full w-full items-center justify-center text-gray-500 ${isExpanded ? 'hidden' : 'flex'}`}
+            className={`flex h-full w-full items-center justify-center text-gray-500 ${isExpanded ? 'hidden' : 'flex sm:hidden'}`}
           >
             {tasks.length > 0 ? (
               <button
