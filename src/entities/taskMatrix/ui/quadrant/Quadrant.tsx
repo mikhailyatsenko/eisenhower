8339b6a4422c846357bce26080742fdf5069fd3b
mikhailyatsenko <misha.ggg@gmail.com>
2025-02-27 11:57:01 +0100

    wip

--- src/entities/taskMatrix/ui/quadrant/Quadrant.tsx
+++ src/entities/taskMatrix/ui/quadrant/Quadrant.tsx
@@ -12,7 +12,7 @@ import { TaskItem } from '../taskItem/TaskItem';
 interface CategoryBlockProps {
   quadrantKey: MatrixKey;
   isDragOver: boolean;
-  isAnimateQuadrants: boolean;
+  isAnimateByExpandQuadrant: boolean;
   tasks: Task[];
   expandedQuadrant: MatrixKey | null;
   handleToggleExpand: (quadrant: MatrixKey) => void;
@@ -47,7 +47,7 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
   quadrantKey,
   isDragOver,
   expandedQuadrant,
-  isAnimateQuadrants,
+  isAnimateByExpandQuadrant,
   handleToggleExpand,
   orderIndex,
   isTypingNewTask,
@@ -58,34 +58,41 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
     data: { quadrantKey },
   });
 
-  const isExpanded = expandedQuadrant === quadrantKey && !isTypingNewTask;
+  const isExpandedCurrentQuadrant =
+    expandedQuadrant === quadrantKey && !isTypingNewTask;
   const recentlyAddedQuadrant = useUIStore(getRecentlyAddedQuadrant);
 
-  const baseStyles = `${quadrantStyles[quadrantKey]} ${recentlyAddedQuadrant === quadrantKey ? 'animate-recently-added-quadrant' : ''} ${isDragOver ? '!bg-gray-400' : ''}`;
-  const typingStyles = isTypingNewTask
-    ? `!h-[calc((100vw)/2-32px)] transition-[width] duration-300 sm:h-[calc((100vw-100vw/6)/2-32px)] ${orderIndex === 0 ? 'animate-from-bottom-appear !w-[calc(55%-8px)]' : '!w-[calc(45%-8px)] !opacity-25'}`
-    : '';
-  const defaultStyles =
-    !isTypingNewTask && expandedQuadrant === null
-      ? 'h-[calc((100vw)/2-32px)] w-[calc(50%-8px)] sm:h-[calc((100vw-100vw/6)/2-32px)]'
-      : isExpanded
+  const actionStyles = isTypingNewTask
+    ? `${orderIndex === 0 ? 'animate-from-bottom-appear w-[calc(55%-8px)] h-[calc((100vw)/2-32px)] sm:h-[calc(100vh/2)] transition-[width] duration-300' : 'w-[calc(45%-8px)] !opacity-25 h-[calc((100vw)/2-32px)] sm:h-[calc(100vh/2)] transition-[width] duration-300'}`
+    : expandedQuadrant === null
+      ? 'w-[calc(50%-8px)] h-[calc((100vw)/2-32px)] sm:h-[calc(100vh/2)]'
+      : isExpandedCurrentQuadrant
         ? '!order-first max-h-[calc(100dvh-550px)] min-h-40 w-full !pb-0'
         : 'h-[calc(100vw/3-48px)] w-[calc((33.333%-8px))]';
 
-  const animateStyles = isAnimateQuadrants ? 'animate-from-hide-to-show' : '';
+  const animateByExpandQuadrant = isAnimateByExpandQuadrant
+    ? 'animate-from-hide-to-show'
+    : '';
+
+  const animateByRecentlyAddedQuadrant =
+    recentlyAddedQuadrant === quadrantKey
+      ? 'animate-recently-added-quadrant'
+      : '';
+
+  const dragOverStyles = isDragOver ? '!bg-gray-400' : '';
 
   return (
     <div
       ref={setNodeRef}
       style={{ order: orderIndex }}
-      className={`${baseStyles} ${typingStyles} ${defaultStyles} relative m-1 overflow-hidden rounded-md p-1 pt-4 text-gray-100 ease-in-out sm:p-6 dark:border dark:bg-black ${animateStyles}`}
+      className={`${quadrantStyles[quadrantKey]} ${actionStyles} ${animateByRecentlyAddedQuadrant} ${animateByExpandQuadrant} ${dragOverStyles} relative m-1 overflow-hidden rounded-md p-1 pt-4 text-gray-100 ease-in-out sm:p-6 dark:border dark:bg-gray-950`}
     >
       <h2 className="absolute top-1 right-2 mb-2 text-[0.5rem] text-gray-600 sm:text-sm dark:text-gray-300">
         {titleMap[quadrantKey]}
       </h2>
       <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
         <ul
-          className={`relative z-2 list-none flex-col ${isExpanded ? 'flex pb-8' : 'hidden'} h-full overflow-x-hidden overflow-y-auto sm:flex`}
+          className={`relative z-2 list-none flex-col ${isExpandedCurrentQuadrant ? 'flex pb-8' : 'hidden'} h-full overflow-x-hidden overflow-y-auto sm:flex`}
         >
           {tasks.map((task, index) => (
             <TaskItem
@@ -102,7 +109,7 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
         </p>
 
         <div
-          className={`relative z-2 flex h-full w-full items-center justify-center text-gray-500 ${isExpanded ? 'hidden' : 'flex sm:hidden'}`}
+          className={`relative z-2 flex h-full w-full items-center justify-center text-gray-500 ${isExpandedCurrentQuadrant ? 'hidden' : 'flex sm:hidden'}`}
         >
           {!isTypingNewTask && tasks.length > 0 ? (
             <button
@@ -116,7 +123,7 @@ export const Quadrant: React.FC<CategoryBlockProps> = ({
           )}
         </div>
       </SortableContext>
-      {isExpanded && (
+      {isExpandedCurrentQuadrant && (
         <button
           onClick={() => handleToggleExpand(quadrantKey)}
           className={`absolute bottom-0 left-0 z-3 flex h-8 w-full cursor-pointer items-center justify-center bg-gray-500 opacity-60 hover:opacity-85 dark:bg-gray-300`}
