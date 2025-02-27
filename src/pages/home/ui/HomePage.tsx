310735821cb062af3eb2fe52c6052d017bfaaf5f
mikhailyatsenko <misha.ggg@gmail.com>
2025-02-27 14:41:35 +0100

    edit buttons styles on edit task

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -2,7 +2,7 @@ import { AddTask } from '@/features/addTask';
 import { InteractWithMatrix } from '@/features/interactWithMatrix';
 export const HomePage = () => {
   return (
-    <div className="mx-auto w-[calc(100%-24px)] p-6 lg:w-5/6">
+    <div className="mx-auto w-[calc(100%-24px)] lg:w-5/6 lg:p-6">
       {/* w-[calc(100%-24px)] because we have names of lines at the left with absolute position */}
       <AddTask />
       <InteractWithMatrix />
