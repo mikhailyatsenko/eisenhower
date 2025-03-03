f4820fdb828f0f45b7981c1c377501fc8fa3b222
mikhailyatsenko <misha.ggg@gmail.com>
2025-03-03 12:05:02 +0100

    make widget taskMatrix

--- src/entities/taskMatrix/index.ts
+++ src/entities/taskMatrix/index.ts
@@ -4,4 +4,3 @@ export { getAllTasks } from './model/selectors/tasksSelector';
 
 export { MatrixQuadrants } from './model/consts/taskMatrixConsts';
 export type { MatrixKey } from './model/types/taskMatrixTypes';
-export { TaskMatrix } from './ui/taskMatrix/TaskMatrix';
