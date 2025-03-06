a5e6fb5cd70eecfef837a759b23cd7ea285fec74
mikhailyatsenko <misha.ggg@gmail.com>
2025-03-06 14:03:35 +0100

    wip

--- src/entities/authIndicator/ui/AuthIndicator.tsx
+++ src/entities/authIndicator/ui/AuthIndicator.tsx
@@ -1,63 +1,93 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import CloseIcon from '@/shared/icons/close-icon.svg';
 import GoogleIcon from '@/shared/icons/google-icon.svg';
+import { SignWihGoogleButton } from '@/shared/ui/signWihGoogleButton';
 
 export const AuthIndicator = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
   };
+
+  const handleEscKey = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setIsOpen(false);
+    }
+  };
+
+  const handleScroll = () => {
+    setIsOpen(false);
+    if (window.scrollY >= 50) {
+      setIsHidden(true);
+    } else {
+      setIsHidden(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('keydown', handleEscKey);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscKey);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscKey);
+    };
+  }, [isOpen]);
 
   return (
     <div
-      className={`fixed top-0 left-0 z-20 overflow-hidden bg-indigo-200/95 transition-all duration-300 [clip-path:_circle(100%_at_0_0)] dark:bg-indigo-950/95 ${isOpen ? 'h-100 w-100 p-5 shadow-lg' : 'h-12 w-12 cursor-pointer'}`}
-      onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
+      ref={ref}
+      className={`fixed top-0 left-0 z-20 overflow-hidden bg-indigo-200/95 transition-all duration-300 [clip-path:_circle(100%_at_0_0)] dark:bg-indigo-950/95 ${isOpen ? 'h-90 w-90 p-5 shadow-lg' : 'h-12 w-12 cursor-pointer hover:h-14 hover:w-14 hover:p-1'} ${isHidden ? '-translate-y-full' : 'translate-y-0'}`}
+      onClick={() => setIsOpen(true)}
     >
       {isOpen ? (
-        <div className="flex flex-col">
+        <div className="flex h-full flex-col">
           <p className="mb-2 w-full">
-            Your matrix is currently stored only on this device. To create a
-            cloud matrix accessible from different devices, sign in with Google
+            Your matrix is currently stored{' '}
+            <strong>only on this device.</strong>
           </p>
-          <div className="flex w-[95%] justify-center">
-            <button className="cursor-pointer px-3 py-1" onClick={handleToggle}>
-              <a
-                href="#"
-                className="mt-4 flex transform items-center justify-center rounded-lg border text-gray-600 transition-colors duration-300 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
-              >
-                <div className="px-4 py-2">
-                  <svg className="h-6 w-6" viewBox="0 0 40 40">
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#FFC107"
-                    />
-                    <path
-                      d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-                      fill="#FF3D00"
-                    />
-                    <path
-                      d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-                      fill="#4CAF50"
-                    />
-                    <path
-                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-                      fill="#1976D2"
-                    />
-                  </svg>
-                </div>
+          <p className="mb-4 w-[98%]">
+            To create a cloud matrix accessible from different devices,{' '}
+            <strong>sign in with Google</strong>
+          </p>
 
-                <span className="w-5/6 px-4 py-3 text-center font-bold">
-                  Sign in with Google
-                </span>
-              </a>
+          <div className="flex w-[95%]">
+            <SignWihGoogleButton />
+          </div>
+
+          <div className="mt-auto w-1/5 opacity-40">
+            <button
+              className="cursor-pointer"
+              onClick={(event) => {
+                event.stopPropagation();
+                setIsOpen(false);
+              }}
+            >
+              <CloseIcon className="fill-foreground h-10 w-10" />
             </button>
           </div>
         </div>
       ) : (
-        <div className="flex h-full w-full p-0.5" onClick={handleToggle}>
+        <div className="flex h-full w-full p-0.5">
           <GoogleIcon className="fill-foreground h-8 w-8" />
         </div>
       )}
---
+++ src/shared/ui/signWihGoogleButton/index.ts
@@ -0,0 +1,1 @@
+export { SignWihGoogleButton } from './SignWihGoogleButton';
