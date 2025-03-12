d508daf894169aa5ac62a25c8d76db3c472911a9
mikhailyatsenko <misha.ggg@gmail.com>
2025-03-12 16:05:22 +0100

    add test for Buttons.tsx

---
+++ src/entities/quadrant/ui/quadrant/__tests__/Buttons.test.tsx
@@ -0,0 +1,73 @@
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+// eslint-disable-next-line boundaries/element-types
+import { MatrixKey } from '@/entities/Tasks/@x/matrixKey';
+import { Buttons } from '../Buttons'; // Импортируйте компонент из нужного файла
+
+describe('Buttons component', () => {
+  const handleToggleExpand = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders expand button when not expanded', () => {
+    const { getByText } = render(
+      <Buttons
+        isExpandedCurrentQuadrant={false}
+        quadrantKey={'ImportantUrgent' as MatrixKey}
+        handleToggleExpand={handleToggleExpand}
+      />,
+    );
+
+    expect(getByText('Expand')).toBeInTheDocument();
+  });
+
+  it('renders collapse button when expanded', () => {
+    const { getByText } = render(
+      <Buttons
+        isExpandedCurrentQuadrant={true}
+        quadrantKey={'ImportantUrgent' as MatrixKey}
+        handleToggleExpand={handleToggleExpand}
+      />,
+    );
+
+    expect(getByText('Collapse Quadrant')).toBeInTheDocument();
+  });
+
+  it('calls handleToggleExpand on button click', () => {
+    const { getByText } = render(
+      <Buttons
+        isExpandedCurrentQuadrant={false}
+        quadrantKey={'ImportantUrgent' as MatrixKey}
+        handleToggleExpand={handleToggleExpand}
+      />,
+    );
+
+    const button = getByText('Expand');
+    fireEvent.click(button);
+
+    expect(handleToggleExpand).toHaveBeenCalledTimes(1);
+    expect(handleToggleExpand).toHaveBeenCalledWith(
+      'ImportantUrgent' as MatrixKey,
+    );
+  });
+
+  it('calls handleToggleExpand on collapse button click', () => {
+    const { getByText } = render(
+      <Buttons
+        isExpandedCurrentQuadrant={true}
+        quadrantKey={'ImportantUrgent' as MatrixKey}
+        handleToggleExpand={handleToggleExpand}
+      />,
+    );
+
+    const button = getByText('Collapse Quadrant');
+    fireEvent.click(button);
+
+    expect(handleToggleExpand).toHaveBeenCalledTimes(1);
+    expect(handleToggleExpand).toHaveBeenCalledWith(
+      'ImportantUrgent' as MatrixKey,
+    );
+  });
+});
