8ab6b610fab07e87296c0207ccb219dc9901f9dc
mikhailyatsenko <misha.ggg@gmail.com>
2026-04-01 11:34:56 +0200

    feat: make delayed GA script

--- src/shared/lib/HeadScripts.tsx
+++ src/shared/lib/HeadScripts.tsx
@@ -2,18 +2,45 @@ import Script from 'next/script';
 
 export const HeadScripts = () => (
   <>
-    <Script
-      strategy="afterInteractive"
-      src="https://www.googletagmanager.com/gtag/js?id=G-GY83Q99SMG"
+    <link
+      rel="preconnect"
+      href="https://www.googletagmanager.com"
+      crossOrigin="anonymous"
     />
-    <Script />
-
-    <Script id="google-analytics" strategy="afterInteractive">
-      {` window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-GY83Q99SMG');`}
+    <Script id="google-analytics-delayed" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        
+        var gtagLoaded = false;
+        
+        function loadGoogleAnalytics() {
+          if (gtagLoaded) return;
+          gtagLoaded = true;
+          
+          var script = document.createElement('script');
+          script.async = true;
+          script.src = 'https://www.googletagmanager.com/gtag/js?id=G-GY83Q99SMG';
+          document.head.appendChild(script);
+          
+          script.onload = function() {
+            gtag('js', new Date());
+            gtag('config', 'G-GY83Q99SMG');
+          };
+        }
+        
+        function onUserInteraction() {
+          if (gtagLoaded) return;
+          loadGoogleAnalytics();
+        }
+        
+        ['mousemove', 'scroll', 'touchstart', 'keydown'].forEach(function(event) {
+          document.addEventListener(event, onUserInteraction, { once: true });
+        });
+        
+        // Backup run after 4 seconds if user does nothing
+        setTimeout(loadGoogleAnalytics, 4000);
+      `}
     </Script>
   </>
 );
