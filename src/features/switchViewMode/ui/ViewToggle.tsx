d919ad088c2f65fdae3d883677ca3995a456a119
mikhailyatsenko <misha.ggg@gmail.com>
2026-04-29 14:10:24 +0200

    fix: ui

--- src/features/switchViewMode/ui/ViewToggle.tsx
+++ src/features/switchViewMode/ui/ViewToggle.tsx
@@ -31,25 +31,25 @@ export const ViewToggle: React.FC = () => {
             width="16"
             height="16"
             rx="3"
-            className="stroke-emerald-600 stroke-2 dark:stroke-emerald-200"
+            className="stroke-gray-500 stroke-2 dark:stroke-gray-400"
             strokeWidth="2"
           />
           <path
             d="M16 10L8 10"
-            className="stroke-emerald-600 stroke-2 dark:stroke-emerald-200"
+            className="stroke-gray-500 stroke-2 dark:stroke-gray-400"
             strokeWidth="2"
             strokeLinecap="round"
           />
           <path
             d="M16 14L8 14"
-            className="stroke-emerald-600 stroke-2 dark:stroke-emerald-200"
+            className="stroke-gray-500 stroke-2 dark:stroke-gray-400"
             strokeWidth="2"
             strokeLinecap="round"
           />
         </svg>
       ) : (
         <svg
-          className="h-12 w-12 fill-green-700 dark:fill-green-300"
+          className="h-12 w-12 fill-gray-500 dark:fill-gray-400"
           viewBox="0 0 28 28"
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +65,7 @@ export const ViewToggle: React.FC = () => {
           >
             <g
               id="ic_fluent_grid_28_filled"
-              className="fill-emerald-600 dark:fill-emerald-200"
+              className="fill-gray-500 dark:fill-gray-400"
               fillRule="nonzero"
             >
               <path d="M10.75,15 C11.9926407,15 13,16.0073593 13,17.25 L13,22.75 C13,23.9926407 11.9926407,25 10.75,25 L5.25,25 C4.00735931,25 3,23.9926407 3,22.75 L3,17.25 C3,16.0073593 4.00735931,15 5.25,15 L10.75,15 Z M22.75,15 C23.9926407,15 25,16.0073593 25,17.25 L25,22.75 C25,23.9926407 23.9926407,25 22.75,25 L17.25,25 C16.0073593,25 15,23.9926407 15,22.75 L15,17.25 C15,16.0073593 16.0073593,15 17.25,15 L22.75,15 Z M10.75,3 C11.9926407,3 13,4.00735931 13,5.25 L13,10.75 C13,11.9926407 11.9926407,13 10.75,13 L5.25,13 C4.00735931,13 3,11.9926407 3,10.75 L3,5.25 C3,4.00735931 4.00735931,3 5.25,3 L10.75,3 Z M22.75,3 C23.9926407,3 25,4.00735931 25,5.25 L25,10.75 C25,11.9926407 23.9926407,13 22.75,13 L17.25,13 C16.0073593,13 15,11.9926407 15,10.75 L15,5.25 C15,4.00735931 16.0073593,3 17.25,3 L22.75,3 Z"></path>
